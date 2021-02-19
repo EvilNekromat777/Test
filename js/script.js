10f21d9b959b0853b4a71b8bed97dc10fd443ee3
Evgeniya Kireeva <evil-nekromat777@mail.ru>
2021-02-19 08:42:03 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,15 +12,15 @@ buttonItemsDelete.forEach((buttonItem) => buttonItem.addEventListener('click', r
 
 $(document).ready(function() {
 	$('.item__sum_minus').click(function () {
-		 var $input = $(this).parent().find('input');
-		 var count = parseInt($input.val()) - 1;
+		 let $input = $(this).parent().find('input');
+		 let count = parseInt($input.val()) - 1;
 		 count = count < 1 ? 1 : count;
 		 $input.val(count);
 		 $input.change();
 		 return false;
 	});
 	$('.item__sum_plus').click(function () {
-		 var $input = $(this).parent().find('input');
+		 let $input = $(this).parent().find('input');
 		 $input.val(parseInt($input.val()) + 1);
 		 $input.change();
 		 return false;
